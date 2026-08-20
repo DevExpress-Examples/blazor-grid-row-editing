f7ec73bf5cbcc66b11e5d60d3944fd15dafb66c4
Svetlana Mikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2026-08-20 16:07:12 +0400

    rewrite example without automapper (#3)

--- EditRow/Data/Northwind/Product.cs
+++ EditRow/Data/Northwind/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -10,11 +11,22 @@ namespace EditRow.Data.Northwind {
         }
 
         public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(50, ErrorMessage = "Must be under 50 characters")]
         public string ProductName { get; set; }
         public int? SupplierId { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
         public int? CategoryId { get; set; }
+
+        [StringLength(100, ErrorMessage = "Must be under 100 characters")]
         public string QuantityPerUnit { get; set; }
+
+        [Range(0, 10000, ErrorMessage = "Must be between 0 and 10000$")]
         public decimal? UnitPrice { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Must be between 0 and 1000")]
         public short? UnitsInStock { get; set; }
         public short? UnitsOnOrder { get; set; }
         public short? ReorderLevel { get; set; }
